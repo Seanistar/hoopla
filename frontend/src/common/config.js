dd0d99800b3b094b25a22a1c733fbbcdd08f6323
SEAN <seankim@crossangle.io>
2020-05-31 00:42:39 +0900

    release api_url

--- frontend/src/common/config.js
+++ frontend/src/common/config.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:3000/api'
-// export const API_URL = 'http://13.124.96.236:3000/api'
+// export const API_URL = 'http://localhost:3000/api'
+export const API_URL = 'http://13.124.96.236/api'
 // export const API_URL = 'http://45.77.24.97:3000/api'
